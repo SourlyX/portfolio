d4bb28f31226380aaaa4b728c236f1c869e808f3
LuisUreña <luis.urenaarosemena@ucr.ac.cr>
2023-07-14 11:25:54 -0600

    whatever

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Navbar from './Navbar'
 import Container from './Container';
 import { Helmet } from 'react-helmet';
 import styled from 'styled-components';
+import { Routes, Route, Link, useLocation } from "react-router-dom";
 import "./index.css"
 
 const MainApp = styled.div`
@@ -38,7 +39,14 @@ function App() {
         <link href="https://fonts.googleapis.com/css2?family=Lato:ital,wght@0,400;0,700;1,400;1,700&display=swap" rel="stylesheet"/>
       </Helmet>
       <Navbar items={navbarItems}/>
-      <Container cards={cards} />
+      <Routes>
+        <Route path="/" element={
+          <div>
+            <Container cards={cards} />
+          </div>}/>
+        <Route path="/about" element={<div></div>}/>
+        <Route path="*" element="404: ruta no encontrada"/>
+      </Routes>
     </MainApp>
   )
 }
